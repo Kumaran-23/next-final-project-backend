1297fff0708ffbb426ec254c9dbd9157745a2740
“chelseahojx” <120448546+chelseahojx@users.noreply.github.com>
2023-07-06 15:32:32 +0800

    initial implementation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.post("/schedule/create", (req, res) => {
+    const { userId, timezone, schedule } = req.body;
+    console.log(req.body);
+});
+
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 
