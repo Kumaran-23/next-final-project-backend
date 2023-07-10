cd008bc8f9d12a8257a192ce1a3f8ac17524b104
“chelseahojx” <120448546+chelseahojx@users.noreply.github.com>
2023-07-10 10:25:56 +0800

    implemented availability endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
 import providerRouter from "./src/controllers/provider.controllers.js"
+import availabilityRouter from "./src/controllers/availability.controllers.js"
 
 const app = express()
 app.use(express.json())
@@ -11,5 +12,6 @@ app.use(cors())
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/providers', providerRouter);
+app.use('/availability', availabilityRouter);
 
 export default app
--- src/controllers/availability.controllers.js
+++ src/controllers/availability.controllers.js
@@ -1,4 +1,27 @@
-app.post("/schedule/create", (req, res) => {
-    const { userId, timezone, schedule } = req.body;
-    console.log(req.body);
+import express from "express"
+import prisma from "../utils/prisma.js"
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    try {
+      const { providerId, day, startAt, endAt } = req.body;
+      console.log(req.body)
+  
+      const availability = await prisma.provider_Avalibility.create({
+        data: {
+          provider_id: providerId,
+          day: day,
+          start_at: startAt,
+          end_at: endAt,
+        },
+      });
+  
+      res.status(200).json({ message: "Availability created", availability });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error creating availability" });
+    }
 });
+
+export default router
