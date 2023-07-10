8ffc666a30721fa754c03508896d6ef90c1a831e
“chelseahojx” <120448546+chelseahojx@users.noreply.github.com>
2023-07-10 17:06:54 +0800

    fixed bugs around availability endpoint

--- src/controllers/availability.controllers.js
+++ src/controllers/availability.controllers.js
@@ -4,24 +4,30 @@ import prisma from "../utils/prisma.js"
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    try {
-      const { providerId, day, startAt, endAt } = req.body;
-      console.log(req.body)
-  
-      const availability = await prisma.provider_Avalibility.create({
+  try {
+    const { providerId, availability } = req.body;
+    console.log(req.body);
+
+    for (const entry of availability) {
+      const { day, startAt, endAt } = entry;
+
+      await prisma.provider_Avalibility.create({
         data: {
-          provider_id: providerId,
           day: day,
           start_at: startAt,
           end_at: endAt,
-        },
+          provider: {
+            connect: { id: parseInt(providerId) },
+          },
+        }
       });
-  
-      res.status(200).json({ message: "Availability created", availability });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error creating availability" });
-    }
+    };
+    
+    res.status(200).json({ message: "Availability created", availability });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating availability" });
+  }
 });
 
 export default router
